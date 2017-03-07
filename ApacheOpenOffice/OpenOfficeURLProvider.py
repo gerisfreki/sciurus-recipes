66982bb28cdf1cd7fe0159691855181c066f774d
Christian Riehl <c.riehl@adcell.de>
2017-03-07 08:53:33 +0100

    OpenOffice download fix

--- ApacheOpenOffice/OpenOfficeURLProvider.py
+++ ApacheOpenOffice/OpenOfficeURLProvider.py
@@ -30,7 +30,7 @@ re_URL = re.compile(r'\<strong\>(?P<the_url>http://.+)\</strong\>')
 re_link = re.compile(r'<a href="(.*?)">.*?</a>', re.IGNORECASE)
 
 # Download link format: Apache_OpenOffice_<version>_MacOS_x86_install_en-US.dmg
-re_dmg = re.compile(r'a[^>]* href="(?P<filename>Apache_OpenOffice_[\d]+.*_MacOS_x86-64_install.+\.dmg)"', re.IGNORECASE)
+re_dmg = re.compile(r'a[^>]* href="(?PApache_OpenOffice_[\d]+.*_MacOS_x86-64_install.+.dmg)"', re.IGNORECASE)
 re_version = re.compile(r'(?P<version>[0-9]+\.[0-9]+\.[0-9]+).*')
 
 
@@ -62,9 +62,9 @@ class OpenOfficeURLProvider(Processor):
             f.close()
         except BaseException as e:
             raise ProcessorError("Can't download %s: %s" % (LOCATOR_URL, e))
-        
+
         m = re_URL.search(html)
-        
+
         if not m:
             raise ProcessorError(
                                  "Couldn't find closest mirror link in %s" % LOCATOR_URL)
@@ -82,12 +82,12 @@ def versions_at_url(self, url):
             f.close()
     except BaseException as e:
         raise ProcessorError("Can't download %s: %s" % (url, e))
-        
+
         links = re.findall(re_link, mirrorhtml)
         if not links:
             raise ProcessorError(
                                  "Couldn't find links in %s" % url)
-        
+
         found_versions = []
         for link_name in links:
             m = re_version.search(link_name)
@@ -98,13 +98,13 @@ def versions_at_url(self, url):
 
 # Sort the versions
 found_versions.sort(key=StrictVersion)
-    
+
     # Reverse the array so the first item is the latest item
     found_versions.reverse()
-        
+
         return found_versions
-    
-    
+
+
     def dmg_link_at_url(self, url):
         """Checks the given URL for a link to OpenOffice installer"""
         try:
@@ -116,7 +116,7 @@ found_versions.sort(key=StrictVersion)
             # Return None instead and the requester will move on to the next URL
             self.output("Can't download %s: %s" % (url, e))
             return None
-    
+
         m = re_dmg.search(html)
         if not m:
             raise ProcessorError(
@@ -128,17 +128,17 @@ found_versions.sort(key=StrictVersion)
 
 def get_openoffice_dmg_url(self, base_url, language_code):
     """Find and return a download URL"""
-        
+
         # Determine the closest mirror or use the override base_url
         if base_url == LOCATOR_URL:
             closest_mirror = self.get_closest_apache_mirror()
     else:
         self.output("Using mirror %s" % base_url)
             closest_mirror = base_url
-        
+
         # Get a list of available versions from the mirror
         versions = self.versions_at_url(closest_mirror)
-        
+
         # Did we get anything?
         if len(versions) == 0:
             raise ProcessorError("Couldn't find any versions")
@@ -146,12 +146,12 @@ def get_openoffice_dmg_url(self, base_url, language_code):
     # Go through each version and check if there's something to download. The versions
     # list is already sorted by version with the latest version at index 0.
     for version in versions:
-        
+
         # Create an URL for the file list page. The URL format appears is:
         # http://<mirror_URL>/<version>/binaries/<language_code>/
         url_pieces = [closest_mirror, version, 'binaries', language_code]
             version_url = '/'.join(s.strip('/') for s in url_pieces)
-            
+
             # If the URL is valid and contains a valid dmg file, bail out and return it
             dmg_url = self.dmg_link_at_url(version_url)
             if dmg_url:
@@ -166,8 +166,8 @@ def get_openoffice_dmg_url(self, base_url, language_code):
 #     propagated to all mirrors.
 raise ProcessorError(
                      "Found one or more versions but couldn't find any download links for language %s" % language_code)
-    
-    
+
+
     def main(self):
         base_url = self.env.get("base_url", LOCATOR_URL)
         language_code = self.env.get("language_code", LANGUAGE_CODE)
