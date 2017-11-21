8c8aad385bab0e93b25003114975fef097b9fda4
Christian Riehl <c.riehl@adcell.de>
2017-11-21 11:04:07 +0100

    fix

--- Opera/OperaURLProvider.py
+++ Opera/OperaURLProvider.py
@@ -44,13 +44,11 @@ class OperaURLProvider(Processor):
     def get_opera_url(self, url):
         try:
             page = urllib2.urlopen(url).read()
-            links = re.findall("[0-9]+.*", page)
-            # links = re.findall("<a.*?\s*href=\"(.*?)\".*?>", page)
+            links = re.findall("<a.*?\s*href=\"(.*?)\".*?>", page)
             url += max(links) + "mac/"
 
             page = urllib2.urlopen(url).read()
-            links = re.findall("[0-9]+.*", page)
-            # links = re.findall("<a.*?\s*href=\"(.*?)\".*?>", page)
+            links = re.findall("<a.*?\s*href=\"(.*?)\".*?>", page)
             for link in links:
                 if ".dmg" in link:
                     url += link
